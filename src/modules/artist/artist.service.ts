6a390a5511631782b20ea7e858d1d552c27a73e4
igor2000xp <igorring@gmail.com>
2022-07-31 21:47:34 +0300

    refactor: refactoring and cleaning code artist.service.ts

--- src/modules/artist/artist.service.ts
+++ src/modules/artist/artist.service.ts
@@ -1,7 +1,11 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
-// import { InMemoryUserStore } from '../../store/in-memory-user-store';
 import {
   createArtist,
   isFieldsExist,
@@ -13,7 +17,6 @@ import { PrismaService } from '../../prisma/prisma.service';
 @Injectable()
 export class ArtistService {
   constructor(private prisma: PrismaService) {}
-  // private store: InMemoryUserStore,
 
   async create(createArtistDto: CreateArtistDto) {
     try {
@@ -48,24 +51,14 @@ export class ArtistService {
   }
 
   async remove(id: string) {
-    // validateId404(id, await this.prisma.artist.findMany());
-    // return await this.prisma.artist.delete({ where: { id } });
-
-    const isArtistFavs = await this.prisma.favorites.findFirst({ where: { id } });
+    const isArtistFavs = await this.prisma.favorites.findFirst({
+      where: { id },
+    });
     if (isArtistFavs) await this.prisma.favorites.delete({ where: { id } });
 
     const isArtist = await this.prisma.artist.findFirst({ where: { id } });
     if (isArtist) return await this.prisma.artist.delete({ where: { id } });
 
     throw new NotFoundException();
-    // this.store.tracks.forEach((track, index) => {
-    //   if (track.artistId === id) this.store.tracks[index].artistId = null;
-    // });
-    //
-    // this.store.favorites.artists = this.store.favorites.artists.filter(
-    //   (itemId) => itemId !== id,
-    // );
-
-    // return `This action removes a #${id} artist`;
   }
 }
