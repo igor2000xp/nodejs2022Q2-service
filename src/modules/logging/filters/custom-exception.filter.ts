10638bd554c3095ec2edfee6c968a722098877fd
igor2000xp <igorring@gmail.com>
2022-08-07 21:27:33 +0300

    feat: implement custom filter and clean

--- src/modules/logging/filters/custom-exception.filter.ts
+++ src/modules/logging/filters/custom-exception.filter.ts
@@ -7,22 +7,6 @@ import {
 } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 
-// @Catch(HttpException)
-// export class CustomExceptionFilter implements ExceptionFilter {
-//   // catch(exception: T, host: ArgumentsHost) {
-//   catch(exception: HttpException, host: ArgumentsHost) {
-//     const ctx = host.switchToHttp();
-//     const response = ctx.getResponse<Response>();
-//     const request = ctx.getRequest<Request>();
-//     const status = exception.getStatus();
-//
-//     response.status(status).json({
-//       statusCode: status,
-//       timestamp: new Date().toISOString(),
-//       path: request.url,
-//     });
-//   }
-// }
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
