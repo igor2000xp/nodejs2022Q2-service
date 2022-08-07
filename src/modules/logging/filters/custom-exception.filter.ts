f4393da242e11f073e66858c14adbb093ed61de0
igor2000xp <igorring@gmail.com>
2022-08-07 21:26:57 +0300

    feat: implement custom filter

---
+++ src/modules/logging/filters/custom-exception.filter.ts
@@ -0,0 +1,48 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+// @Catch(HttpException)
+// export class CustomExceptionFilter implements ExceptionFilter {
+//   // catch(exception: T, host: ArgumentsHost) {
+//   catch(exception: HttpException, host: ArgumentsHost) {
+//     const ctx = host.switchToHttp();
+//     const response = ctx.getResponse<Response>();
+//     const request = ctx.getRequest<Request>();
+//     const status = exception.getStatus();
+//
+//     response.status(status).json({
+//       statusCode: status,
+//       timestamp: new Date().toISOString(),
+//       path: request.url,
+//     });
+//   }
+// }
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = {
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
--- src/modules/logging/filters/custom-exeption.filter.ts
+++
@@ -1,6 +0,0 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-
-@Catch()
-export class CustomExeptionFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
-}
