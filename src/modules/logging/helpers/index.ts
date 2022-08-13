31ac524d8ca2bf6d973a90c1fd5e49943b53b384
igor2000xp <igorring@gmail.com>
2022-08-13 13:29:40 +0300

    fix: clean some commented code

--- src/modules/logging/helpers/index.ts
+++ src/modules/logging/helpers/index.ts
@@ -1,11 +1,6 @@
 import { LogLevel } from '@nestjs/common';
 import * as fs from 'fs/promises';
 import * as path from 'path';
-// import { CustomExceptionsFilter } from '../filters/custom-exception.filter';
-// import { MyLoggingService } from '../my-logging.service';
-import { UnknownElementException } from '@nestjs/core/errors/exceptions/unknown-element.exception';
-// import { MyLoggingService } from '../my-logging.service';
-// import * as log from '../my-logging.service';
 
 export const getLevel = (logLevel: string): LogLevel[] => {
   switch (logLevel) {
@@ -18,80 +13,32 @@ export const getLevel = (logLevel: string): LogLevel[] => {
   }
 };
 
-// const fileName = '';
-// const fileExtension = '';
-// const fileSize = 1024 * 1024;
-
-export const updateFile = async (
-  message: string,
-  context: string,
-  fileName: string,
-  // fileSize: number,
-  // fileExtension: string,
-) => {
-  const filePath = path.resolve(process.cwd(), process.env.LOG_FILE_FOLDER);
-  // try {
-  // const file = await fs.stat(path.resolve('logs', fileName));
-  // if (!file || file.size + message.length > fileSize) {
-  //   fileName = setFileName(context, fileExtension);
-  //
-  try {
-    await fs.appendFile(
-      path.resolve(filePath, fileName),
-      `${message} [${context}] \n\n`,
-      { flag: 'a', encoding: 'utf8' },
-    );
-  } catch (err) {
-    console.log(err);
-  }
-  // }
-  // } catch (err) {
-  //   const message = `Fail to open file ${fileName}`;
-  //   console.log(err + ' - ' + message);
-  // }
-};
-
 export const setFileName = (
   context,
   fileExtension: string,
   filePrefix: string,
 ): string => {
-  // const type = context === 'LoggingServiceMiddleware' ? 'logs' : 'errors';
-  // return `${type}_${Date.now()}.${fileExtension}`;
   return `${filePrefix}_${Date.now()}.${fileExtension}`;
 };
 
 export const createAndWriteFile = async (
   message: string,
   context: string,
-  // isNewFile = true,
   fileNameAndExt: string,
-  fileExtension: string,
-  filePrefix: string,
 ) => {
-  const filePath = path.resolve(process.cwd(), 'logs');
+  const filePath = path.resolve(process.cwd(), process.env.LOG_FILE_FOLDER);
   try {
     await fs.lstat(filePath);
   } catch (err) {
     await fs.mkdir(filePath, { recursive: true });
   }
-  if (!fileNameAndExt) {
-    fileNameAndExt = setFileName(context, fileExtension, filePrefix);
-    // console.log(fileNameAndExt);
-  }
   try {
-    try {
-      // await fs.lstat(path.resolve(filePath, fileNameAndExt));
-      await fs.appendFile(
-        path.resolve(filePath, fileNameAndExt),
-        `${message} [${context}] \n\n`,
-        { flag: 'a', encoding: 'utf8' },
-      );
-    } catch (err) {
-      console.log('Error file appending');
-    }
-    // console.log(path.resolve(filePath, fileNameAndExt), `${message}`, 'utf8');
+    await fs.appendFile(
+      path.resolve(filePath, fileNameAndExt),
+      `${message} [${context}] \n\n`,
+      { flag: 'a', encoding: 'utf8' },
+    );
   } catch (err) {
-    throw new UnknownElementException('File writing Error');
+    console.log('Error file appending');
   }
 };
--- src/modules/logging/my-logging.service.ts
+++ src/modules/logging/my-logging.service.ts
@@ -1,11 +1,6 @@
 import { ConsoleLogger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {
-  createAndWriteFile,
-  getLevel,
-  setFileName,
-  updateFile,
-} from './helpers';
+import { createAndWriteFile, getLevel, setFileName } from './helpers';
 import * as fs from 'fs/promises';
 import * as path from 'path';
 
@@ -15,7 +10,7 @@ export class MyLoggingService extends ConsoleLogger {
   public logFileFolder: string;
   private readonly contextMiddleWare: string;
   private logFileName = '';
-  private errorFileName = '';
+  // private errorFileName = '';
   private readonly fileExtension: string;
 
   constructor(private config: ConfigService) {
@@ -29,111 +24,74 @@ export class MyLoggingService extends ConsoleLogger {
   }
 
   async log(message: string, context: string) {
-    // context = this.contextMiddleWare;
     super.log.apply(this, [`${message}`, context, this.contextMiddleWare]);
-    // console.log(message, 'log', context, '!!!!!!!!!!!!!!!!!!');
     await this.writeLogFile(message, 'log', context);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async error(message: string, context?: string) {
-    // if (this.logLevel < 2) return;
-
     super.error.apply(this, [`${message}`, this.contextMiddleWare]);
-    // this.writeLogToFile(message, 'error', context);
-    // console.log(message, 'log', context, '!!!!!!!!!!!!!!!!!!');
     // await this.writeLogToFile(message, 'error', context);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   warn(message: string, context?: string) {
-    // if (this.logLevel < 3) return;
-
     super.warn.apply(this, [`${message}`, this.contextMiddleWare]);
     // this.writeLogToFile(message, 'warn', context);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   debug(message: string, context?: string) {
     super.debug.apply(this, [`${message}`, this.contextMiddleWare]);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   verbose(message: string, context?: string) {
     super.verbose.apply(this, [`${message}`, this.contextMiddleWare]);
   }
 
   async writeLogFile(message: string, filePrefix: string, context?: string) {
-    let isUpdate = true;
     let readFileSize: number;
     const filePath = path.resolve(process.cwd(), this.logFileFolder);
-    // console.log(path.resolve(filePath, this.logFileName));
+    const newContext = this.trueContext(this.context, context);
     if (!this.logFileName) {
-      this.logFileName = setFileName(this.context, this.fileExtension, filePrefix);
+      this.logFileName = setFileName(
+        this.context,
+        this.fileExtension,
+        filePrefix,
+      );
     } else {
       try {
-        // console.log(path.resolve('logs', this.logFileName));
-        // **************
         const fileStat = await fs.stat(
-          path.resolve('logs', this.logFileName),
+          path.resolve(filePath, this.logFileName),
         );
         readFileSize = fileStat.size;
-        // console.log('file');
       } catch (err) {
-        isUpdate = false;
         console.log('file is absent');
       }
 
       if (readFileSize + message.length > this.fileSize) {
-        // console.log(readFileSize, message.length, this.fileSize);
-        // isUpdate = false;
-        this.logFileName = setFileName(context, this.fileExtension, filePrefix);
+        this.logFileName = setFileName(
+          newContext,
+          this.fileExtension,
+          filePrefix,
+        );
       }
-
-      // console.log(readFileSize, message.length, this.fileSize, '***********', path.resolve(process.cwd(), 'logs', this.logFileName));
-      // console.log(message, filePrefix, this.logFileName, isUpdate, context);
-      await this.writeToFile(
-        message,
-        filePrefix,
-        this.logFileName,
-        isUpdate,
-        context,
-      );
+      await this.writeToFile(message, filePrefix, this.logFileName, newContext);
     }
+  }
+
+  trueContext(thisContext: string, context?: string) {
+    return context ? `[${context}]` : this.context ? `[${this.context}]` : '';
   }
 
   async writeToFile(
     message: string,
     filePrefix: string,
     fileNameAndExt: string,
-    isUpdate: boolean,
-    context?: string,
+    context: string,
   ) {
-    const newContext = context
-      ? `[${context}]`
-      : this.context
-      ? `[${this.context}]`
-      : '';
-    const fileMessage = `${filePrefix.toUpperCase()} ${newContext} ${message} \n`;
-    console.log(!isUpdate);
-    if (!isUpdate) {
-      console.log('createAndWriteFile');
-      const isNewFile = true;
-      //   const fileNameAndExt = setFileName(this.context, this.fileExtension);
-      //   fileName = fileNameAndExt;
-
-      await createAndWriteFile(
-        fileMessage,
-        newContext,
-        // isNewFile,
-        fileNameAndExt,
-        this.fileExtension,
-        filePrefix,
-      );
-    } else {
-      await updateFile(
-        message,
-        newContext,
-        fileNameAndExt,
-        // this.fileSize,
-        // this.fileExtension,
-      );
-    }
+    const fileMessage = `${filePrefix.toUpperCase()} ${context} ${message} \n`;
+    await createAndWriteFile(fileMessage, context, fileNameAndExt);
   }
 }
